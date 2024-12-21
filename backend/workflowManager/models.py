44d72fab170a359a86a40d20cefb0c370fdffe8e
Akash Kumar Singh <akashkumarsingh214@gmail.com>
2024-12-21 13:01:26 +0530

    View added

--- backend/workflowManager/models.py
+++ backend/workflowManager/models.py
@@ -15,41 +15,6 @@ class Agent(models.Model):
     # Description of the AI agent
     description = models.TextField()
 
-    def __str__(self):
-        # String representation of the Agent object
-        return f"Agent(id={self.id}, name={self.name}, description={str(self.description)[:20]}{'...' if len(self.description) > 20 else ''})"
-
-
-class WorkflowElement(models.Model):
-    # Primary key for the WorkflowElement model
-    id = models.AutoField(primary_key=True)
-    # Workflow ID associated with the element
-    workflow_id = models.CharField(max_length=255)
-    # Position of the element in the workflow
-    position = models.IntegerField()
-    # Agent ID associated with the element
-    agent_id = models.CharField(max_length=255)
-    # Name of the workflow element
-    name = models.CharField(max_length=255)
-    # Description of the workflow element
-    description = models.TextField()
-    # Prompt for relevancy checking
-    relevancy_checking_prompt = models.TextField()
-
-    def __str__(self):
-        # String representation of the WorkflowElement object
-        return (
-            f"WorkflowElement(id={self.id}, "
-            f"workflow_id={self.workflow_id}, "
-            f"position={self.position}, "
-            f"agent_id={self.agent_id}, "
-            f"name={self.name}, "
-            f"description={str(self.description)[:20]}..., "
-            f"relevancy_checking_prompt={str(self.relevancy_checking_prompt)[:20]}...)"
-            if len(self.relevancy_checking_prompt) > 20
-            else f"relevancy_checking_prompt={self.relevancy_checking_prompt})"
-        )
-
 
 # Workflow model
 class Workflow(models.Model):
@@ -60,16 +25,36 @@ class Workflow(models.Model):
     # Description of the workflow
     description = models.TextField()
 
-    def __str__(self):
-        # String representation of the Workflow object
-        return (
-            f"Workflow(id={self.id}, "
-            f"name={self.name}, "
-            f"description={str(self.description)[:20]}{'...' if len(self.description) > 20 else ''})"
-        )
+
+class WorkflowElement(models.Model):
+    # Primary key for the WorkflowElement model
+    id = models.AutoField(primary_key=True)
+    # Workflow ID associated with the element
+    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    # Position of the element in the workflow
+    position = models.IntegerField()
+    # Agent ID associated with the element
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    # Name of the workflow element
+    name = models.CharField(max_length=255)
+    # Description of the workflow element
+    description = models.TextField()
+    # Prompt for relevancy checking
+    relevancy_checking_prompt = models.TextField()
 
 
 class Document(models.Model):
+    # Primary key for the Document model
+    id = models.AutoField(primary_key=True)
+    # Version of the document
+    latest_version = models.IntegerField()
+    # Workflow associated with the document
+    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    # Owner of the document
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class VersionedDocument(models.Model):
     # Primary key for the Document model
     id = models.AutoField(primary_key=True)
     # Title of the document
@@ -80,20 +65,8 @@ class Document(models.Model):
     version = models.IntegerField()
     # Mapping of workflow element ID to corresponding document content
     workflow_elements = models.JSONField()
-    # Owner of the document
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        # String representation of the Document object
-        return (
-            f"Document(id={self.id}, "
-            f"title={self.title}, "
-            f"creation_time={self.creation_time}, "
-            f"version={self.version}, "
-            f"workflow_elements={str(self.workflow_elements)[:20]}"
-            f"{'...' if len(str(self.workflow_elements)) > 20 else ''}, "
-            f"owner={self.owner.username if self.owner else 'N/A'})"
-        )
+    # Foreign key to the Document model
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
 
 
 class ChatMessage(models.Model):
@@ -106,7 +79,7 @@ class ChatMessage(models.Model):
     # Primary key for the Chat model
     id = models.AutoField(primary_key=True)
     # Document ID associated with the chat
-    doc_id = models.CharField(max_length=255)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
     # Type of the agent sending the message (agent or user)
     from_agent_type = models.CharField(max_length=5, choices=AGENT_TYPE_CHOICES)
     # ID of the agent sending the message
@@ -117,8 +90,8 @@ class ChatMessage(models.Model):
     creation_time = models.DateTimeField(auto_now_add=True)
     # Content of the message
     message = models.TextField()
-    # Current document content
-    current_document = models.TextField()
+    # Foreign key to the VersionedDocument model
+    current_document = models.ForeignKey(VersionedDocument, on_delete=models.CASCADE)
     # Type of the agent receiving the message (agent or user)
     to_agent_type = models.CharField(max_length=5, choices=AGENT_TYPE_CHOICES)
     # ID of the agent receiving the message
@@ -128,22 +101,6 @@ class ChatMessage(models.Model):
     # End position in the document
     end_position = models.IntegerField()
     # Current workflow element ID
-    current_workflow_element = models.IntegerField()
-
-    def __str__(self):
-        # String representation of the Chat object
-        return (
-            f"Chat(id={self.id}, "
-            f"doc_id={self.doc_id}, "
-            f"from_agent_type={self.from_agent_type}, "
-            f"from_id={self.from_id}, "
-            f"in_reply_to={self.in_reply_to}, "
-            f"creation_time={self.creation_time}, "
-            f"message={str(self.message)[:20]}{'...' if len(self.message) > 20 else ''}, "
-            f"current_document={str(self.current_document)[:20]}..., "
-            f"to_agent_type={self.to_agent_type}, "
-            f"to_id={self.to_id}, "
-            f"start_position={self.start_position}, "
-            f"end_position={self.end_position}, "
-            f"current_workflow_element={self.current_workflow_element})"
-        )
+    current_workflow_element = models.ForeignKey(
+        WorkflowElement, on_delete=models.CASCADE
+    )
--- backend/workflowManager/views.py
+++ backend/workflowManager/views.py
@@ -1,3 +1,88 @@
+import json
+
+import agents.agent_factory as AgentFactory
+from agents.types import AgentType, LLMResponse
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import *
+
 
 # Create your views here.
+@csrf_exempt
+def create_document(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "Invalid request method"}, status=400)
+    data = json.loads(request.body)
+    document = Document.objects.create(workflow=data["workflow"])
+    return JsonResponse(
+        {"id": document.id, "title": document.title, "content": document.content}
+    )
+
+
+@csrf_exempt
+def send_chat_message(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "Invalid request method"}, status=400)
+    data = json.loads(request.body)
+    chat_message = ChatMessage.objects.create(message=data["message"])
+    return JsonResponse({"id": chat_message.id, "message": chat_message.message})
+
+
+def fetch_chat_messages(document):
+    return ChatMessage.objects.filter(document=document)
+
+
+def process_chat_message(chat_message):
+    assign_workflow_element(chat_message)
+    chat_message.save()
+    document = chat_message.document
+    chat_messages = fetch_chat_messages(document)
+    llmresponse = AgentFactory.create_agent(
+        chat_message.current_workflow_element.agent
+    ).process(chat_messages)
+
+
+def handle_llm_response(
+    llmresponse: LLMResponse, document: Document, workflow_element: WorkflowElement
+):
+    updated_content = llmresponse.updated_workflow_doc
+    document.latest_version += 1
+    prevoius_version_document = VersionedDocument.objects.filter(
+        document=document
+    ).latest("version")
+    latest_version_document = prevoius_version_document
+    latest_version_document.version = document.latest_version
+    latest_version_document.workflow_elements[workflow_element.id] = updated_content
+    latest_version_document.id = None
+    latest_version_document.save()
+    document.save()
+    chat_message = ChatMessage.objects.create(
+        document=document,
+        message=llmresponse.response_message,
+        from_agent_type=AgentType.AGENT,
+        from_id=workflow_element.agent.id,
+        to_id=request.user.id,
+        to_agent_type=AgentType.USER,
+        current_document=latest_version_document,
+    )
+    chat_message.save()
+    if llmresponse.move_to_next_workflow:
+        document.workflow = document.workflow.next_workflow
+        document.save()
+    return chat_message
+
+
+def assign_workflow_element(chat_message):
+    document = chat_message.document
+    chat_messages = fetch_chat_messages(document)
+    workflow = document.workflow
+    workflow_elements = WorkflowElement.objects.filter(workflow=workflow)
+    chat_message.current_workflow_element = get_workflow_element(
+        document, chat_message, chat_messages, workflow_elements
+    )
+
+
+def get_workflow_element(document, chat_message, chat_messages, workflow_elements):
+    return workflow_elements[0]
